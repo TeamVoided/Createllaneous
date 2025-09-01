1e81b556e76dd025bed2791ae48f69de229cf489
TheEnderCore <theendercore@gmail.com>
2025-09-01 15:16:38 +0300

    why?

--- src/main/kotlin/org/teamvoided/createllaneous/utils/Helpers.kt
+++ src/main/kotlin/org/teamvoided/createllaneous/utils/Helpers.kt
@@ -7,12 +7,9 @@ import net.minecraft.world.entity.Entity
 import net.minecraft.world.entity.EntityType
 import net.minecraft.world.item.Item
 import net.minecraft.world.level.block.Block
-import java.util.*
 
 fun EntityType<*>.isIn(tag: TagKey<EntityType<*>>) = `is`(tag)
 fun Entity.isIn(tag: TagKey<EntityType<*>>) = type.isIn(tag)
 
-fun blockKey(block: Block): ResourceLocation =
-    Objects.requireNonNull(BuiltInRegistries.BLOCK.getKey(block))
-fun itemKey(item: Item): ResourceLocation =
-    Objects.requireNonNull(BuiltInRegistries.ITEM.getKey(item))
+fun blockKey(block: Block): ResourceLocation = BuiltInRegistries.BLOCK.getKey(block)
+fun itemKey(item: Item): ResourceLocation = BuiltInRegistries.ITEM.getKey(item)
