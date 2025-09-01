9d6a88e1a4fb25e7b9ead8947b368b5cb323fa63
TheEnderCore <theendercore@gmail.com>
2025-09-01 15:16:56 +0300

    extra tag gen

--- src/main/kotlin/org/teamvoided/createllaneous/data/gen/prov/tag/CMItemTagProvider.kt
+++ src/main/kotlin/org/teamvoided/createllaneous/data/gen/prov/tag/CMItemTagProvider.kt
@@ -4,6 +4,8 @@ package org.teamvoided.createllaneous.data.gen.prov.tag
 import net.minecraft.core.HolderLookup
 import net.minecraft.data.PackOutput
 import net.minecraft.data.tags.ItemTagsProvider
+import net.minecraft.tags.BlockTags
+import net.minecraft.tags.ItemTags
 import net.minecraft.world.level.block.Block
 import org.teamvoided.createllaneous.Createllaneous.MODID
 import org.teamvoided.createllaneous.data.gen.FH
@@ -14,5 +16,12 @@ class CMItemTagProvider(o: PackOutput, l: Lookup, bt: CompletableFuture<TagLooku
     ItemTagsProvider(o, l, bt, MODID, FH) {
     override fun addTags(provider: HolderLookup.Provider) {
 //        tag(AllTags.AllItemTags.UPRIGHT_ON_BELT.tag).add()
+        copy()
+    }
+
+    fun copy() {
+        copy(BlockTags.SLABS, ItemTags.SLABS)
+        copy(BlockTags.STAIRS, ItemTags.STAIRS)
+        copy(BlockTags.TRAPDOORS, ItemTags.TRAPDOORS)
     }
 }
